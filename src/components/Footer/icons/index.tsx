b2ff41b49d929cd15b4fb7a8421c837ff9aabd17
gustavolim4 <gustavolll15567@gmail.com>
2024-12-23 19:39:29 -0300

    tirei o botao do saiba mais sobre nois

--- src/components/Footer/icons/index.tsx
+++ src/components/Footer/icons/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { FaWhatsapp } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
+import { FaTiktok } from "react-icons/fa6";
 import "./styles.css";
 
 const IconsFooter = () => {
@@ -21,6 +22,11 @@ const IconsFooter = () => {
         <li>
           <a href="#dadasdada">
             <FaInstagram />
+          </a>
+        </li>
+        <li>
+          <a href="#dadasdada">
+            <FaTiktok /> 
           </a>
         </li>
       </ul>
--- src/components/SobreFt/caixaTxt/index.tsx
+++ src/components/SobreFt/caixaTxt/index.tsx
@@ -1,16 +1,11 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import { useLayoutEffect } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import "./style.css";
 
 const CaixaTxt = () => {
-  const navigate = useNavigate();
 
-  const PaginaSobre = () => {
-    navigate("/sobre"); // Caminho para onde você quer navegar
-  };
 
   useLayoutEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
@@ -45,9 +40,6 @@ const CaixaTxt = () => {
           transformar vidas através da atividade física e do autocuidado. Venha
           fazer parte dessa jornada!
         </p>
-        <div className="buttionTxt">
-          <button onClick={PaginaSobre}>Sabia mais sobre nós</button>
-        </div>
       </div>
     </div>
   );
