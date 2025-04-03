319de4ab4833f3aa56a8c06e532e4ab55afdf865
gustavoLim4 <gustavolll15567@gmail.com>
2025-04-02 21:08:00 -0300

    manda para master

--- src/store/index.ts
+++ src/store/index.ts
@@ -16,4 +16,5 @@ export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
+
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
